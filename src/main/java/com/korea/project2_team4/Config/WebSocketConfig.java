5d63940d5ed6b7c8280586f04ea61a2ac68c77d5
unknown <qhs3012@gmail.com>
2024-01-10 18:40:32 +0900

    노트북수정제대로

--- src/main/java/com/korea/project2_team4/Config/WebSocketConfig.java
+++ src/main/java/com/korea/project2_team4/Config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.korea.project2_team4;
+package com.korea.project2_team4.Config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -9,11 +9,8 @@ function setConnected(connected) {
     else {
         $("#conversation").hide();
     }
-<<<<<<< HEAD
-    $("#greetings").html("");
-=======
     $("#savemessages").html("");
->>>>>>> 5f915483e7b5317df0fc7afa14f58ff4fc9f94cf
+
 }
 
 function connect() {
@@ -23,13 +20,8 @@ function connect() {
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
-<<<<<<< HEAD
-        stompClient.subscribe('/topic/greetings', function (greeting) {
-            showGreeting(JSON.parse(greeting.body).content);
-=======
         stompClient.subscribe('/topic/messaging', function (messaging) {
             showMessaging(JSON.parse(messaging.body).content);
->>>>>>> 5f915483e7b5317df0fc7afa14f58ff4fc9f94cf
         });
     });
 }
@@ -42,21 +34,13 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-<<<<<<< HEAD
-function sendName() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
-}
 
-function showGreeting(message) {
-    $("#greetings").append("<tr><td>" + message + "</td></tr>");
-=======
 function sendContent() {
     stompClient.send("/app/hello", {}, JSON.stringify({'content': $("#content").val()}));
 }
 
 function showMessaging(message) {
     $("#savemessages").append("<tr><td>" + message + "</td></tr>");
->>>>>>> 5f915483e7b5317df0fc7afa14f58ff4fc9f94cf
 }
 
 $(function () {
@@ -65,9 +49,5 @@ $(function () {
     });
     $( "#connect" ).click(function() { connect(); });
     $( "#disconnect" ).click(function() { disconnect(); });
-<<<<<<< HEAD
-    $( "#send" ).click(function() { sendName(); });
-=======
     $( "#send" ).click(function() { sendContent(); });
->>>>>>> 5f915483e7b5317df0fc7afa14f58ff4fc9f94cf
 });
